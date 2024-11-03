d53c21ae7c8857df1873868cc0cfab4cf702a803
Benjamin Aaron <ben@theaarons.com>
2024-11-03 15:18:00 -0800

    respond to ping (poorly)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("Connection established!");
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("Connection failed: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let mut buff = [0; 512];
+    stream.read(&mut buff).unwrap();
+
+    println!("Recieved: {:?}", buff);
+
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
