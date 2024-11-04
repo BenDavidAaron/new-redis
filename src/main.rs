715e9d4a863d89c3ce1371834ad7f9ec5f861fe9
Benjamin Aaron <ben@theaarons.com>
2024-11-03 20:54:22 -0800

    refactor ping

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use crate::resp::RESP;
+use crate::resp::{bytes_to_resp, RESP};
+use crate::server::process_request;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 mod resp;
 mod resp_result;
+mod server;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -28,7 +30,21 @@ async fn handle_connection(mut stream: TcpStream) {
     loop {
         match stream.read(&mut buff).await {
             Ok(size) if size != 0 => {
-                let response = RESP::SimpleString(String::from("PONG"));
+                let mut index: usize = 0;
+                let request = match bytes_to_resp(&buff[..size].to_vec(), &mut index) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        eprintln!("Error: {}", e);
+                        return;
+                    }
+                };
+                let response = match process_request(request) {
+                    Ok(v) => v,
+                    Err(_) => {
+                        eprintln!("Error processing request");
+                        return;
+                    }
+                };
                 if let Err(e) = stream.write_all(response.to_string().as_bytes()).await {
                     eprintln!("Error writing to socket: {}", e);
                 }
